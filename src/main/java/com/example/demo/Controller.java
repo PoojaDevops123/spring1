1833d0ebe22b3f993fe8f377804ec62faf795c05
Pooji <pooji@gmail.com>
2022-11-09 12:28:08 +0530

    1

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class Controller {
+
+	// One syntax to implement a
+	// GET method
+	@GetMapping("/")
+	public String home()
+	{
+		String str
+			= "<html><body><font color=\"black\">"
+			+ "<h1>atomstate Welcomes You.!</h1>"
+			+ "</font></body></html>";
+		return str;
+	}
+
+	// Another syntax to implement a
+	// GET method
+	@RequestMapping("/test")
+	public String info()
+	{
+		String str2
+			= "<html><body><font color=\"red\">"
+			+ "<h2>TEST Route Tested"+"</h2></font></body></html>";
+		return str2;
+	}
+}
